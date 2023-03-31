1aed5376faabf218206c7fd7b1eeecedf73dbbc9
RahulB <rahulbisht1012@gmail.com>
2023-03-31 11:26:12 +0530

    README update

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # FlipKart Clone
 __________________________
+
+
+
+## 🎯 Learnings from the Projects:
+
+ ### 📌 About Material UI
+ __________________________
+* You can directly style the components of material UI with this syntax:
+
+
+* we can use this Syntax for Custom css of HTMl tag in React and material UI
+
+
+`css
+const PlusImg = styled('img')({
+    width: 12,
+    height: 12,
+    marginLeft: 4
+})
+`
+
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,22 +1,53 @@
 import React from 'react'
 
-import { AppBar, Toolbar, Box, styled } from '@mui/material'
+import { AppBar, Toolbar, Box, Typography, styled } from '@mui/material'
 
+// Custom css for material UI Components
 const StyledHeader = styled(AppBar)`
 background:#2874f0;
+height:55px
 `
 
-const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+const CustomBox = styled(Box)`
+margin-left:12%;
+line-height:0;
+
+`
+
+const SubHeading = styled(Typography)`
+font-size:12px;
+font-style:italic;
+`
+
+const PlusImg = styled('img')({
+    width: 12,
+    height: 12,
+    marginLeft: 4
+})
+
 
 
 const Header = () => {
+
+    const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+    const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
+
     return (
         <>
             <StyledHeader>
                 <Toolbar>
-                    <Box>
-                        <img src={logoURL} alt="Flipkart Logo" />
-                    </Box>
+                    <CustomBox>
+                        <img src={logoURL} alt="Flipkart Logo"
+                            style={{ width: 80 }}
+                        />
+                        <Box style={{ display: "flex" }}>
+                            <SubHeading>
+                                Explore &nbsp;
+                                <Box component="span" style={{ color: 'yellow', fontWeight: "bold" }}>Plus</Box>
+                            </SubHeading>
+                            <PlusImg src={subURL} alt="" />
+                        </Box>
+                    </CustomBox>
                 </Toolbar>
             </StyledHeader>
         </>
